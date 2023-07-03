c0e1f1a60030c010f9daf20e1e4e0f141cd25a36
Gorilja11 <130550660+Gorilja11@users.noreply.github.com>
2023-07-03 22:44:18 +0200

    Initial Commit

---
+++ SOFA/SOFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOFAApp.swift
+//  SOFA
+//
+//  Created by Anders Gilja on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SOFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
